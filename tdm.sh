1c8e88be703161f59ea48d4e09ccf37e076c7d86
Julien Chastang <julien.c.chastang@gmail.com>
2016-08-26 17:14:59 -0600

    TDM_HOME env variable

--- tdm.sh
+++ tdm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-java -d64 -Xmx6g -DbbTdm=1 -Dtds.content.root.path=/opt/tomcat/content \
-     -jar tdm-$TDM_VERSION.jar -nthreads 1 -cred tdm:$TDM_PW -tds $TDS_HOST
+java -jar -d64 -Xmx6g -DbbTdm=1 -Dtds.content.root.path=$HOME/content \
+      $TDM_HOME/tdm-$TDM_VERSION.jar -nthreads 1 -cred \
+      tdm:$TDM_PW -tds $TDS_HOST
