af3482d2a2cf180a47ea33efc28af62397de6447
Julien Chastang <julien.c.chastang@gmail.com>
2016-09-28 15:05:48 -0600

    javaopts reintroduced

---
+++ files/javaopts.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+###
+# Java options
+###
+
+# http://www.unidata.ucar.edu/software/thredds/current/tds/faq.html#javaUtilPrefs
+# Choosing a JAVA_PREFS_SYSTEM_ROOT directory location that will likely live
+# inside the container.
+
+NORMAL="-server -d64 -Xms4G -Xmx4G"
+HEAP_DUMP="-XX:+HeapDumpOnOutOfMemoryError"
+HEADLESS="-Djava.awt.headless=true"
+CONTENT_ROOT="-Dtds.content.root.path=$CATALINA_HOME/content"
+JAVA_PREFS_SYSTEM_ROOT="-Djava.util.prefs.systemRoot=$CATALINA_HOME/javaUtilPrefs -Djava.util.prefs.userRoot=$CATALINA_HOME/javaUtilPrefs"
+
+JAVA_OPTS="$JAVA_OPTS $CONTENT_ROOT/ $JAVA_PREFS_SYSTEM_ROOT $NORMAL $HEAP_DUMP $HEADLESS"
+export JAVA_OPTS
