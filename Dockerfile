Unidata/thredds-docker

commit af3482d2a2cf180a47ea33efc28af62397de6447

javaopts reintroduced

Dockerfile
@@ -50,27 +50,20 @@ COPY files/threddsConfig.xml ${CATALINA_HOME}/content/thredds/threddsConfig.xml
 COPY files/tomcat-users.xml ${CATALINA_HOME}/conf/tomcat-users.xml
 
 ###
+# Tomcat Java Options
+###
+
+COPY files/setenv.sh $CATALINA_HOME/bin/setenv.sh
+
+COPY files/javaopts.sh $CATALINA_HOME/bin/javaopts.sh
+
+RUN chmod 755 $CATALINA_HOME/bin/*.sh
+
+###
 # Expose ports
 ###
 
 EXPOSE 8080 8443
-
-###
-# Java options
-###
-
-# http://www.unidata.ucar.edu/software/thredds/current/tds/faq.html#javaUtilPrefs
-# Choosing a JAVA_PREFS_SYSTEM_ROOT directory location that will likely live
-# inside the container.
-
-RUN mkdir -p ${CATALINA_HOME}/javaUtilPrefs/.systemPrefs
-
-ENV JAVA_OPTS -server -d64 -Xms4G -Xmx4G \
-   -XX:+HeapDumpOnOutOfMemoryError \
-   -Djava.awt.headless=true \
-   -Dtds.content.root.path=${CATALINA_HOME}/content \
-   -Djava.util.prefs.systemRoot=${CATALINA_HOME}/javaUtilPrefs \
-   -Djava.util.prefs.userRoot=${CATALINA_HOME}/javaUtilPrefs
 
 ###
 # Reasserting ownership and permissions from parent container

files/javaopts.sh
@@ -1,0 +1,18 @@
+#!/bin/sh
+
+###
+# Java options
+###
+
+# http://www.unidata.ucar.edu/software/thredds/current/tds/faq.html#javaUtilPrefs
+# Choosing a JAVA_PREFS_SYSTEM_ROOT directory location that will likely live
+# inside the container.
+
+NORMAL="-server -d64 -Xms4G -Xmx4G"
+HEAP_DUMP="-XX:+HeapDumpOnOutOfMemoryError"
+HEADLESS="-Djava.awt.headless=true"
+CONTENT_ROOT="-Dtds.content.root.path=$CATALINA_HOME/content"
+JAVA_PREFS_SYSTEM_ROOT="-Djava.util.prefs.systemRoot=$CATALINA_HOME/javaUtilPrefs -Djava.util.prefs.userRoot=$CATALINA_HOME/javaUtilPrefs"
+
+JAVA_OPTS="$JAVA_OPTS $CONTENT_ROOT/ $JAVA_PREFS_SYSTEM_ROOT $NORMAL $HEAP_DUMP $HEADLESS"
+export JAVA_OPTS

files/setenv.sh
@@ -1,0 +1,5 @@
+#!/bin/sh
+
+if [ -r "$CATALINA_HOME/bin/javaopts.sh" ]; then
+  . "$CATALINA_HOME/bin/javaopts.sh"
+fi

commit 57f7c276ce8f73daeed0322b7cd7ee1b93ba8f1f

Fixing error introduced in af3482d
tomcat writeable directory needed for ncWMS

Dockerfile
@@ -107,6 +107,14 @@ COPY files/javaopts.sh $CATALINA_HOME/bin/javaopts.sh
 RUN chmod 755 $CATALINA_HOME/bin/*.sh
 
 ###
+# Creating .systemPrefs directory according to 
+# http://www.unidata.ucar.edu/software/thredds/current/tds/faq.html#javaUtilPrefs
+# and as defined in the files/javaopts.sh file
+###
+
+RUN mkdir -p ${CATALINA_HOME}/javaUtilPrefs/.systemPrefs
+
+###
 # Expose ports
 ###
 