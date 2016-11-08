97750abb24e814ea983ded4b5f24fae3f693430e
Julien Chastang <julien.c.chastang@gmail.com>
2016-11-08 14:13:11 -0700

    accidentally testing expected instead of actual

--- .travis/test.sh
+++ .travis/test.sh
@@ -2,6 +2,5 @@
 
 curl -o ./.travis/actual.html http://127.0.0.1:8080/thredds/catalog.html && \
 echo toplevel catalog.html OK && \
-grep 'THREDDS Data Server' ./.travis/expected.html && \
+grep 'THREDDS Data Server' ./.travis/actual.html && \
 echo toplevel catalog.html string content OK
-
