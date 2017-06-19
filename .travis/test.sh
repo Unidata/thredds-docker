c09e4fbc28b45ce424ea849ed0258ca0fbe62c0a
Julien Chastang <julien.c.chastang@gmail.com>
2017-06-19 05:34:55 +0000

    travis 5.0-SNAPSHOT fix

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -o ./.travis/actual.html http://127.0.0.1:8080/thredds/catalog.html && \
+curl -o ./.travis/actual.html http://127.0.0.1:8080/thredds/catalog/catalog.html && \
 echo toplevel catalog.html OK && \
 grep 'THREDDS Data Server' ./.travis/actual.html && \
 echo toplevel catalog.html string content OK
